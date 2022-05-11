df52cd7b35ccfd1eceeea51e8d8cb1a4f5faabc5
Jota Landes <jhonatamalta@hotmail.com>
2022-05-10 22:21:44 -0300

    Add project files.

---
+++ 001-LoginSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _001_LoginSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
